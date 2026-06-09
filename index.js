95cd6077f2e92c54038bb74eb34ac5d57ed50fad
NikhilDarekar <nikhilx97@gmail.com>
2026-06-09 18:25:44 +0530

    calculator

---
+++ index.js
@@ -0,0 +1,31 @@
+const clearBtn = document.getElementById('clear-button')
+const deleteBtn = document.getElementById('delete-button')
+const divideBtn = document.getElementById('divide-button')
+const multiplyBtn = document.getElementById('multiply-button')
+const substractBtn = document.getElementById('substract-button')
+const addBtn = document.getElementById('add-button')
+const decimalBtn = document.getElementById('decimal-button')
+const equalBtn = document.getElementById('equal-button')
+const numberBtns = document.querySelectorAll('.number')
+const resultElement = document.getElementById('result')
+
+//initialize the variables
+let result = '';
+let operation = '';
+let previousOperand = 0;
+
+
+//Append number
+const appendNumber = (number) => {
+    result += number
+    resultElement.innerText = result
+}
+
+//Add event listener to number buttons
+numberBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        // console.log(button.innerText)
+        appendNumber(button.innerText);
+        
+    })
+})
